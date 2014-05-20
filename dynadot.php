ebeb474af2b24808002ba541e0710eaefe48768c
Myles McNamara <myles@smyl.es>
2014-05-20 15:51:00 -0400

    reformat

--- dynadot.php
+++ dynadot.php
@@ -1,43 +1,51 @@
 <?php
- /**
+/**
  * @title               WHMCS Dynadot Registrar Module
  *
  * @author              Myles McNamara (get@smyl.es)
  * @copyright           Copyright (c) Myles McNamara 2014
- * @Date:               5/20/14
- * @Last Modified by:   Myles McNamara
- * @Last Modified time: 20 13 08
+ * @Date                :               5/20/14
+ * @Last                Modified by:   Myles McNamara
+ * @Last                Modified time: 20 13 08
  */
 
-if (!defined("WHMCS"))
-	die("This file cannot be accessed directly");
+if ( !defined( "WHMCS" ) ) {
+	die( "This file cannot be accessed directly" );
+}
 
 require_once( dirname( __FILE__ ) . "/inc/core.php" );
 
-function dynadot_getconfigarray( )
-{
-	$configarray = array( "APIKey" => array( "Type" => "text", "Size" => "20", "Description" => "Put your Dynadot API v3 Key here." ) );
+function dynadot_getconfigarray() {
+	$configarray = array( "APIKey" => array( "Type"        => "text",
+	                                         "Size"        => "20",
+	                                         "Description" => "Put your Dynadot API v3 Key here."
+	)
+	);
+
 	return $configarray;
 }
 
-function dynadot_GetNameservers( $params )
-{
+function dynadot_GetNameservers( $params ) {
 	$dynadot = new WHMCS_Dynadot( $params );
+
 	return $dynadot->getNS();
 }
 
-function dynadot_SaveNameservers( $params ){
+function dynadot_SaveNameservers( $params ) {
 	$dynadot = new WHMCS_Dynadot( $params );
+
 	return $dynadot->saveNS();
 }
 
-function dynadot_RegisterDomain ( $params ){
+function dynadot_RegisterDomain( $params ) {
 	$dynadot = new WHMCS_Dynadot( $params );
+
 	return $dynadot->register();
 }
 
-function dynadot_RenewDomain ( $params ){
+function dynadot_RenewDomain( $params ) {
 	$dynadot = new WHMCS_Dynadot( $params );
+
 	return $dynadot->renew();
 }
 

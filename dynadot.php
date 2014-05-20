87ed120fe3b81e9bd228e544bb817d45d2989ffb
Myles McNamara <myles@smyl.es>
2014-05-20 15:50:22 -0400

    update for release

--- dynadot.php
+++ dynadot.php
@@ -1,89 +1,44 @@
 <?php
+ /**
+ * @title               WHMCS Dynadot Registrar Module
+ *
+ * @author              Myles McNamara (get@smyl.es)
+ * @copyright           Copyright (c) Myles McNamara 2014
+ * @Date:               5/20/14
+ * @Last Modified by:   Myles McNamara
+ * @Last Modified time: 20 13 08
+ */
+
+if (!defined("WHMCS"))
+	die("This file cannot be accessed directly");
+
+require_once( dirname( __FILE__ ) . "/inc/core.php" );
 
 function dynadot_getconfigarray( )
 {
-    $configarray = array( "APIKey" => array( "Type" => "text", "Size" => "20", "Description" => "Put your Dynadot API v3 Key here." ) );
-    return $configarray;
+	$configarray = array( "APIKey" => array( "Type" => "text", "Size" => "20", "Description" => "Put your Dynadot API v3 Key here." ) );
+	return $configarray;
 }
 
-function dynadot_getnameservers( $params )
+function dynadot_GetNameservers( $params )
 {
-    return $values;
+	$dynadot = new WHMCS_Dynadot( $params );
+	return $dynadot->getNS();
 }
 
-function dynadot_savenameservers( $params )
-{
-    $query = "https://api.dynadot.com/api3.xml?key={$params['APIKey']}&command=set_ns&domain={$params['sld']}.{$params['tld']}";
-    if ( $params['ns1'] )
-    {
-        $query .= "&ns0={$params['ns1']}";
-    }
-    if ( $params['ns2'] )
-    {
-        $query .= "&ns1={$params['ns2']}";
-    }
-    if ( $params['ns3'] )
-    {
-        $query .= "&ns2={$params['ns3']}";
-    }
-    if ( $params['ns4'] )
-    {
-        $query .= "&ns3={$params['ns4']}";
-    }
-    $ch = curl_init( );
-    curl_setopt( $ch, CURLOPT_URL, $query );
-    curl_setopt( $ch, CURLOPT_HEADER, 0 );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    $result = curl_exec( $ch );
-    curl_close( $ch );
-
-    $response = simplexml_load_string($result);
-
-    logModuleCall('dynadot', 'dynadot save ns', $query, $result);
-
-    $responsexml = $response->SetNsHeader[0];
-
-    if($responsexml->Status == 'error'){
-    	$error = $responsexml->Error;
-    	logModuleCall('dynadot', 'dynadot save ns error', $responsexml, $error);
-        $values['error'] = $error;
-    }
-
-    return $values;
+function dynadot_SaveNameservers( $params ){
+	$dynadot = new WHMCS_Dynadot( $params );
+	return $dynadot->saveNS();
 }
 
-function dynadot_registerdomain( $params )
-{
-    $query = "https://api.dynadot.com/api3.xml?key={$params['APIKey']}&command=register&domain={$params['sld']}.{$params['tld']}&duration={$params['regperiod']}";
-    $ch = curl_init( );
-    curl_setopt( $ch, CURLOPT_URL, $query );
-    curl_setopt( $ch, CURLOPT_HEADER, 0 );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    $result = curl_exec( $ch );
-    curl_close( $ch );
-    $result = explode( ",", $result );
-    if ( $result[1] != "success" )
-    {
-        $values['error'] = $result[1]." - ".$result[2];
-    }
-    return $values;
+function dynadot_RegisterDomain ( $params ){
+	$dynadot = new WHMCS_Dynadot( $params );
+	return $dynadot->register();
 }
 
-function dynadot_renewdomain( $params )
-{
-    $query = "https://api.dynadot.com/api3.xml?key={$params['APIKey']}&command=renew&domain={$params['sld']}.{$params['tld']}&duration={$params['regperiod']}";
-    $ch = curl_init( );
-    curl_setopt( $ch, CURLOPT_URL, $query );
-    curl_setopt( $ch, CURLOPT_HEADER, 0 );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    $result = curl_exec( $ch );
-    curl_close( $ch );
-    $result = explode( ",", $result );
-    if ( $result[1] != "success" )
-    {
-        $values['error'] = $result[1]." - ".$result[2];
-    }
-    return $values;
+function dynadot_RenewDomain ( $params ){
+	$dynadot = new WHMCS_Dynadot( $params );
+	return $dynadot->renew();
 }
 
 ?>

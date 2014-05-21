3deb105dcfcf5fcdbd87ba3f742160be33b24a24
Myles McNamara <myles@smyl.es>
2014-05-20 20:55:18 -0400

    Update dynadot.php

--- dynadot.php
+++ dynadot.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * @title               WHMCS Dynadot Registrar Module
+ * @title               WHMCS Dynadot Registar Module
  *
  * @author              Myles McNamara (get@smyl.es)
+ * @url                 http://smyl.es
+ * @github              https://github.com/tripflex/whmcs-dynadot
  * @copyright           Copyright (c) Myles McNamara 2014
- * @Date                :               5/20/14
- * @Last                Modified by:   Myles McNamara
- * @Last                Modified time: 20 13 08
+ * @license             GPLv3+
+ * @Date                5/20/14
  */
 
 if ( !defined( "WHMCS" ) ) {
@@ -16,10 +17,12 @@ if ( !defined( "WHMCS" ) ) {
 require_once( dirname( __FILE__ ) . "/inc/core.php" );
 
 function dynadot_getconfigarray() {
-	$configarray = array( "APIKey" => array( "Type"        => "text",
-	                                         "Size"        => "20",
-	                                         "Description" => "Put your Dynadot API v3 Key here."
-	)
+	$configarray = array(
+		"APIKey" => array(
+			"Type"        => "text",
+			"Size"        => "20",
+			"Description" => "Put your Dynadot API v3 Key here."
+		)
 	);
 
 	return $configarray;
